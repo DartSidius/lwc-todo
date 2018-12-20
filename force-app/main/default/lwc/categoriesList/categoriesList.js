5a10349fd760ffb407cc04c79e652e7f3d128249
Vladyslav Lyfar <vlad.lyfar@gmail.com>
2018-12-20 19:53:55 +0200

    create project boilerplate

---
+++ force-app/main/default/lwc/categoriesList/categoriesList.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+
+export default class CategoriesList extends LightningElement {
+    categories = [
+        {
+            Id: 1,
+            label: "My day",
+            icon: "standard:custom_notification"
+        },
+        {
+            Id: 2,
+            label: "Important",
+            icon: "standard:opportunity"
+        },
+        {
+            Id: 3,
+            label: "To do",
+            icon: "standard:proposition"
+        },
+        {
+            Id: 4,
+            label: "Tasks",
+            icon: "standard:task"
+        }
+    ];
+}
